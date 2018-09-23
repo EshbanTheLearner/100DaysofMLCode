06460cea45ce0b0775542cb5b19a9c619109bde0
Eshban Suleman <eshban9492@gmail.com>
2018-09-23 22:04:40 +0500

    Day-1 of 100DaysofMLCode: Simple Linear Regression

---
+++ Day-1/regression.py
@@ -0,0 +1,9 @@
+# Importing libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Importing the dataset
+dataset_day = pd.read_csv('day.csv')
+dataset_hour = pd.read_csv('hour.csv')
